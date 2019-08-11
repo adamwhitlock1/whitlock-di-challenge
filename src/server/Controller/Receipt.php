40f973fe19271d2249cb72576a65bb77583191cc
Adam Whitlock <adam.whitlock627@gmail.com>
2019-08-10 22:09:16 -0600

    some unit tests for samples

--- src/server/Controller/Receipt.php
+++ src/server/Controller/Receipt.php
@@ -6,7 +6,22 @@ namespace App\Controller;
 
 class Receipt
 {
-public function tax($inputAmount, $inputTax){
-    return $inputTax * $inputAmount;
-}
+
+    public function total(array $items, $coupon){
+
+       $sum = array_sum($items);
+       if (!is_null($coupon)){
+           return $sum - ($sum * $coupon);
+       }
+       return $sum;
+    }
+
+    public function tax($inputAmount, $inputTax){
+        return $inputTax * $inputAmount;
+    }
+
+    public function postTaxTotal($items, $tax, $coupon){
+        $subtotal = $this->total($items, $coupon);
+        return $subtotal + $this->tax($subtotal, $tax);
+    }
 }
--- tests/Controller/ReceiptTest.php
+++ tests/Controller/ReceiptTest.php
@@ -25,16 +25,55 @@ protected function tearDown(): void
 }
 
     /** @test */
+    public function testTotal()
+    {
+        $inputItems = [1, 2, 3, 4, 5];
+        $coupon = null;
+        $output = $this->receipt->total($inputItems, $coupon);
+        $this->assertEquals(
+            15,
+            $output,
+            "Total should equal 1.00"
+        );
+    }
+
+    /** @test */
+    public function testTotalAndCoupon()
+    {
+        $inputItems = [1, 2, 3, 4, 5];
+        $coupon = 0.20;
+        $output = $this->receipt->total($inputItems, $coupon);
+        $this->assertEquals(
+            12,
+            $output,
+            "Total should equal 1.00"
+        );
+    }
+
+    /** @test */
     public function testTax()
     {
         $inputAmount = 10.00;
         $inputTax = .10;
         $output = $this->receipt->tax($inputAmount, $inputTax);
+        // echo " ". $output . " ";
         $this->assertEquals(
             1.00,
             $output,
             "Tax calculate should equal 1.00"
         );
+    }
+
+    public function testPostTaxTotal() {
+        $receipt = $this->getMockBuilder('App\Controller\Receipt')
+            ->setMethods(['tax', 'total'])
+            ->getMock();
+        $receipt->method('total')
+            ->will($this->returnValue(10.00));
+        $receipt->method('tax')
+            ->will($this->returnValue(1.00));
+        $result = $receipt->postTaxTotal([1, 2], 0.20, null);
+        $this->assertEquals(11.00, $result);
     }
 }
 
